9089359f37b028c4fa91ac0e66207732656b10a0
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-13 21:51:52 +0100

    fix: add custom emoji

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -553,7 +553,7 @@ export function convertMSToDelayString(ms: number): string {
   return result;
 }
 
-export function successMessage(str: string, emoji = "<:zep_check:906897402101891093>") {
+export function successMessage(str: string, emoji = "<:zep_check:1008109392899621065>") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
