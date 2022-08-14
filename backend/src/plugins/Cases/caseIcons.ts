2e254bbe55eb48df3b168d13c0f1416abb0274d5
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-14 19:00:13 +0100

    fix: use custom emojis for cases

--- backend/src/plugins/Cases/caseIcons.ts
+++ backend/src/plugins/Cases/caseIcons.ts
@@ -3,13 +3,13 @@ import { CaseTypes } from "../../data/CaseTypes";
 // These emoji icons are hosted on the Hangar server
 // If you'd like your self-hosted instance to use these icons, check #add-your-bot on that server
 export const caseIcons: Record<CaseTypes, string> = {
-  [CaseTypes.Ban]: "<:case_ban:906897178176393246>",
-  [CaseTypes.Unban]: "<:case_unban:906897177824067665>",
-  [CaseTypes.Note]: "<:case_note:906897177832476743>",
-  [CaseTypes.Warn]: "<:case_warn:906897177840844832>",
-  [CaseTypes.Kick]: "<:case_kick:906897178310639646>",
-  [CaseTypes.Mute]: "<:case_mute:906897178147057664>",
-  [CaseTypes.Unmute]: "<:case_unmute:906897177819881523>",
-  [CaseTypes.Deleted]: "<:case_deleted:906897178209968148>",
-  [CaseTypes.Softban]: "<:case_softban:906897177828278274>",
+  [CaseTypes.Ban]: "<:case_ban:1008433920024510564>",
+  [CaseTypes.Unban]: "<:case_unban:1008433954795311164>",
+  [CaseTypes.Note]: "<:case_note:1008434165814935623>",
+  [CaseTypes.Warn]: "<:case_warn:1008434226091278366>",
+  [CaseTypes.Kick]: "<:case_kick:1008434264032952372>",
+  [CaseTypes.Mute]: "<:case_mute:1008434285927223406>",
+  [CaseTypes.Unmute]: "<:case_unmute:1008434358287356024>",
+  [CaseTypes.Deleted]: "<:case_deleted:1008434419377373326>",
+  [CaseTypes.Softban]: "<:case_softban:1008434423877877791>",
 };
